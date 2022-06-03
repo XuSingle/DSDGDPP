c40c35146af57f36f5d53ec1d2de7a7d66fbf02a
XuSingle <46293069+XuSingle@users.noreply.github.com>
2022-06-04 00:11:01 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 # python main.py --model 1 --G_gap 5000 --sparse_G 0.3 --sparse_D 0 --model_type G_Only
-python main.py --model 0 --G_gap 5000 --D_gap 5000 --sparse_G 0 --sparse_D 0 --model_type OG
+# python main.py --model 0 --G_gap 5000 --D_gap 5000 --sparse_G 0 --sparse_D 0 --model_type OG
 #python main.py --model 1 --G_gap 5000 --D_gap 5000 --sparse_G 0 --sparse_D 0 --model_type OG
 #python main.py --model 1 --G_gap 5000 --D_gap 5000 --sparse_G 0 --sparse_D 0 --model_type OG
 #python main.py --model 1 --G_gap 5000 --D_gap 5000 --sparse_G 0 --sparse_D 0 --model_type OG
@@ -45,11 +45,16 @@ python main.py --model 0 --G_gap 5000 --D_gap 5000 --sparse_G 0 --sparse_D 0 --m
 #python main.py --model 1 --gap 500 --sparse_G 0.5 --sparse_D 0 --model_type G_Only
 #python main.py --model 1 --gap 500 --sparse_G 0.5 --sparse_D 0 --model_type G_Only
 
-#python main.py --model 1 --gap 1000 --sparse_G 0.1 --sparse_D 0 --model_type G_Only
-#python main.py --model 1 --gap 1000 --sparse_G 0.1 --sparse_D 0 --model_type G_Only
-#python main.py --model 1 --gap 1000 --sparse_G 0.1 --sparse_D 0 --model_type G_Only
-#python main.py --model 1 --gap 1000 --sparse_G 0.1 --sparse_D 0 --model_type G_Only
-#python main.py --model 1 --gap 1000 --sparse_G 0.1 --sparse_D 0 --model_type G_Only
+python main.py --model 1 --gap 1000 --sparse_G 0.1 --sparse_D 0 --model_type G_Only
+python main.py --model 1 --gap 1000 --sparse_G 0.1 --sparse_D 0 --model_type G_Only
+python main.py --model 1 --gap 1000 --sparse_G 0.1 --sparse_D 0 --model_type G_Only
+python main.py --model 1 --gap 1000 --sparse_G 0.1 --sparse_D 0 --model_type G_Only
+python main.py --model 1 --gap 1000 --sparse_G 0.1 --sparse_D 0 --model_type G_Only
+python main.py --model 1 --gap 1000 --sparse_G 0.1 --sparse_D 0 --model_type G_Only
+python main.py --model 1 --gap 1000 --sparse_G 0.1 --sparse_D 0 --model_type G_Only
+python main.py --model 1 --gap 1000 --sparse_G 0.1 --sparse_D 0 --model_type G_Only
+python main.py --model 1 --gap 1000 --sparse_G 0.1 --sparse_D 0 --model_type G_Only
+python main.py --model 1 --gap 1000 --sparse_G 0.1 --sparse_D 0 --model_type G_Only
 #
 #
 #python main.py --model 1 --gap 1000 --sparse_G 0.3 --sparse_D 0 --model_type G_Only
