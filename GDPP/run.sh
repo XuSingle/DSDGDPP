b08b61f06973e2f8a4e578f36ebccfc295e441ee
XuSingle <46293069+XuSingle@users.noreply.github.com>
2022-06-05 22:15:07 +0800

    Update run.sh

--- GDPP/run.sh
+++ GDPP/run.sh
@@ -1,8 +1,8 @@
-python main.py --model 1 --gap 1000 --sparse 0.1
-python main.py --model 1 --gap 1000 --sparse 0.1
-python main.py --model 1 --gap 1000 --sparse 0.1
-python main.py --model 1 --gap 1000 --sparse 0.1
-python main.py --model 1 --gap 1000 --sparse 0.1
+# python main.py --model 1 --gap 1000 --sparse 0.1
+# python main.py --model 1 --gap 1000 --sparse 0.1
+# python main.py --model 1 --gap 1000 --sparse 0.1
+# python main.py --model 1 --gap 1000 --sparse 0.1
+# python main.py --model 1 --gap 1000 --sparse 0.1
 
 
 python main.py --model 1 --gap 1000 --sparse 0.3
@@ -10,3 +10,48 @@ python main.py --model 1 --gap 1000 --sparse 0.3
 python main.py --model 1 --gap 1000 --sparse 0.3
 python main.py --model 1 --gap 1000 --sparse 0.3
 python main.py --model 1 --gap 1000 --sparse 0.3
+
+
+python main.py --model 1 --gap 1000 --sparse 0.5
+python main.py --model 1 --gap 1000 --sparse 0.5
+python main.py --model 1 --gap 1000 --sparse 0.5
+python main.py --model 1 --gap 1000 --sparse 0.5
+python main.py --model 1 --gap 1000 --sparse 0.5
+
+
+python main.py --model 1 --gap 3000 --sparse 0.1
+python main.py --model 1 --gap 3000 --sparse 0.1
+python main.py --model 1 --gap 3000 --sparse 0.1
+python main.py --model 1 --gap 3000 --sparse 0.1
+python main.py --model 1 --gap 3000 --sparse 0.1
+
+python main.py --model 1 --gap 3000 --sparse 0.3
+python main.py --model 1 --gap 3000 --sparse 0.3
+python main.py --model 1 --gap 3000 --sparse 0.3
+python main.py --model 1 --gap 3000 --sparse 0.3
+python main.py --model 1 --gap 3000 --sparse 0.3
+
+python main.py --model 1 --gap 3000 --sparse 0.5
+python main.py --model 1 --gap 3000 --sparse 0.5
+python main.py --model 1 --gap 3000 --sparse 0.5
+python main.py --model 1 --gap 3000 --sparse 0.5
+python main.py --model 1 --gap 3000 --sparse 0.5
+
+
+python main.py --model 1 --gap 5000 --sparse 0.1
+python main.py --model 1 --gap 5000 --sparse 0.1
+python main.py --model 1 --gap 5000 --sparse 0.1
+python main.py --model 1 --gap 5000 --sparse 0.1
+python main.py --model 1 --gap 5000 --sparse 0.1
+
+python main.py --model 1 --gap 5000 --sparse 0.3
+python main.py --model 1 --gap 5000 --sparse 0.3
+python main.py --model 1 --gap 5000 --sparse 0.3
+python main.py --model 1 --gap 5000 --sparse 0.3
+python main.py --model 1 --gap 5000 --sparse 0.3
+
+python main.py --model 1 --gap 5000 --sparse 0.5
+python main.py --model 1 --gap 5000 --sparse 0.5
+python main.py --model 1 --gap 5000 --sparse 0.5
+python main.py --model 1 --gap 5000 --sparse 0.5
+python main.py --model 1 --gap 5000 --sparse 0.5
