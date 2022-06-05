bf942c4fa1a0ec357234b82481f9a55325c32cc5
XuSingle <46293069+XuSingle@users.noreply.github.com>
2022-06-05 21:58:28 +0800

    Update run.sh

--- GDPP/run.sh
+++ GDPP/run.sh
@@ -1,5 +1,12 @@
-python main.py --model 1
-python main.py --model 1
-python main.py --model 1
-python main.py --model 1
-python main.py --model 1
+python main.py --model 1 --gap 1000 --sparse 0.1
+python main.py --model 1 --gap 1000 --sparse 0.1
+python main.py --model 1 --gap 1000 --sparse 0.1
+python main.py --model 1 --gap 1000 --sparse 0.1
+python main.py --model 1 --gap 1000 --sparse 0.1
+
+
+python main.py --model 1 --gap 1000 --sparse 0.3
+python main.py --model 1 --gap 1000 --sparse 0.3
+python main.py --model 1 --gap 1000 --sparse 0.3
+python main.py --model 1 --gap 1000 --sparse 0.3
+python main.py --model 1 --gap 1000 --sparse 0.3
