550fb3248c45539590309888466d9cdb017646b8
XuSingle <46293069+XuSingle@users.noreply.github.com>
2022-06-07 21:55:33 +0800

    Update run0.sh

--- GDPP/run0.sh
+++ GDPP/run0.sh
@@ -1,20 +1,20 @@
-python main.py --model 0 --gap 1000 --sparse 0.1
-python main.py --model 0 --gap 1000 --sparse 0.1
-python main.py --model 0 --gap 1000 --sparse 0.1
-python main.py --model 0 --gap 1000 --sparse 0.1
-python main.py --model 0 --gap 1000 --sparse 0.1
+# python main.py --model 0 --gap 1000 --sparse 0.1
+# python main.py --model 0 --gap 1000 --sparse 0.1
+# python main.py --model 0 --gap 1000 --sparse 0.1
+# python main.py --model 0 --gap 1000 --sparse 0.1
+# python main.py --model 0 --gap 1000 --sparse 0.1
 
-python main.py --model 0 --gap 1000 --sparse 0.3
-python main.py --model 0 --gap 1000 --sparse 0.3
-python main.py --model 0 --gap 1000 --sparse 0.3
-python main.py --model 0 --gap 1000 --sparse 0.3
-python main.py --model 0 --gap 1000 --sparse 0.3
+# python main.py --model 0 --gap 1000 --sparse 0.3
+# python main.py --model 0 --gap 1000 --sparse 0.3
+# python main.py --model 0 --gap 1000 --sparse 0.3
+# python main.py --model 0 --gap 1000 --sparse 0.3
+# python main.py --model 0 --gap 1000 --sparse 0.3
 
-python main.py --model 0 --gap 1000 --sparse 0.5
-python main.py --model 0 --gap 1000 --sparse 0.5
-python main.py --model 0 --gap 1000 --sparse 0.5
-python main.py --model 0 --gap 1000 --sparse 0.5
-python main.py --model 0 --gap 1000 --sparse 0.5
+# python main.py --model 0 --gap 1000 --sparse 0.5
+# python main.py --model 0 --gap 1000 --sparse 0.5
+# python main.py --model 0 --gap 1000 --sparse 0.5
+# python main.py --model 0 --gap 1000 --sparse 0.5
+# python main.py --model 0 --gap 1000 --sparse 0.5
 
 python main.py --model 0 --gap 3000 --sparse 0.1
 python main.py --model 0 --gap 3000 --sparse 0.1
